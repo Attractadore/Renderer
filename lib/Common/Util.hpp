f24cb542ae0a656c272b4436576a5c70ee000e35
Michael <attractadore02@gmail.com>
2022-05-27 01:14:05 +0300

    Remove unused

--- lib/Common/Util.hpp
+++ lib/Common/Util.hpp
@@ -3,19 +3,6 @@
 #include <memory>
 
 namespace R1 {
-#if 0
-// CTAD doesn't work for unique_ptr since T* can represent
-// either a single element or an array
-template<typename T, typename Deleter = std::default_delete<T>>
-class single_unique_ptr: public std::unique_ptr<T, Deleter> {
-public:
-    explicit single_unique_ptr(T* p, Deleter&& d): std::unique_ptr<T, Deleter>(p, std::move(d)) {}
-};
-
-template<typename T, typename Deleter = std::default_delete<T>>
-explicit single_unique_ptr(T*, Deleter&& d) -> single_unique_ptr<T, Deleter>;
-#endif
-
 template<typename T, typename... Us>
 constexpr auto make_array(Us&&... args) {
     return std::array<T, sizeof...(Us)> {
