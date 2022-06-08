7ea905722cdeb66fee41e426a573699ab2a97fb6
Michael <attractadore02@gmail.com>
2022-06-08 15:10:32 +0300

    Save graphics queue in context

--- lib/Rendering/include/Rendering/Context.hpp
+++ lib/Rendering/include/Rendering/Context.hpp
@@ -6,12 +6,24 @@ namespace R1::Rendering {
 class Device;
 
 class Context {
-    HContext m_context;
+    Device&         m_device;
+    HContext        m_context;
+    QueueFamily::ID m_graphics_queue_family;
+    GAPI::Queue     m_graphics_queue;
 
 public:
     Context(Device& device, const ContextConfig& config):
-        m_context{GAPI::CreateContext(device.get(), config)} {}
+        m_device{device},
+        m_context{GAPI::CreateContext(device.get(), config)}
+    {
+        // TODO: this is obviously temporary
+        assert(config.queue_config.size() == 1);
+        m_graphics_queue_family = config.queue_config[0].id;
+        m_graphics_queue = GAPI::GetQueue(m_context.get(), m_graphics_queue_family, 0);
+    }
 
     GAPI::Context get() noexcept { return m_context.get(); }
+    Device& GetDevice() noexcept { return m_device; }
+    GAPI::Queue GetGraphicsQueue() noexcept { return m_graphics_queue; }
 };
 }
