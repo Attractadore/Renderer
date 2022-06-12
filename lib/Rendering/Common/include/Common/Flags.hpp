93cacbc9d17a56327d891b5ff74c1c2aee286938
Michael <attractadore02@gmail.com>
2022-06-12 14:44:55 +0300

    Add flags to replace bitfields

---
+++ lib/Rendering/Common/include/Common/Flags.hpp
@@ -0,0 +1,91 @@
+#pragma once
+#include <type_traits>
+
+namespace R1 {
+namespace Detail {
+struct EmptyFlagsT {};
+}
+inline constexpr Detail::EmptyFlagsT EmptyFlags;
+
+template<typename E>
+    requires std::is_enum_v<E>
+class Flags {
+    E value = E(0);
+
+public:
+    using BitType = E;
+    using UnderlyingType = std::underlying_type_t<E>;
+
+    constexpr Flags(decltype(EmptyFlags)) noexcept {}
+    constexpr Flags(BitType bit) noexcept:
+        value{bit} {}
+
+    constexpr BitType operator&(BitType r) const noexcept {
+        return static_cast<E>(
+            static_cast<UnderlyingType>(value) & static_cast<UnderlyingType>(r)
+        );
+    }
+
+    constexpr Flags<E> operator&(Flags<E> r) const noexcept {
+        using U = std::underlying_type_t<E>;
+        return static_cast<E>(
+            static_cast<U>(value) & static_cast<U>(r.value)
+        );
+    }
+
+    constexpr Flags<E>& operator&=(Flags<E> r) noexcept {
+        return *this = *this & r;
+    }
+
+    constexpr Flags<E> operator|(Flags<E> r) const noexcept {
+        using U = std::underlying_type_t<E>;
+        return static_cast<E>(
+            static_cast<U>(value) | static_cast<U>(r.value)
+        );
+    }
+    
+    constexpr Flags<E>& operator|=(Flags<E> r) noexcept {
+        return *this = *this | r;
+    }
+
+    constexpr bool AnySet(Flags<E> mask) const noexcept {
+        return (*this & mask) != EmptyFlags;
+    }
+
+    constexpr bool AllSet(Flags<E> mask) const noexcept {
+        return (*this & mask) == mask;
+    }
+
+    constexpr bool NoneSet(Flags<E> mask) const noexcept {
+        return (*this & mask) == EmptyFlags;
+    }
+
+    constexpr bool operator==(const Flags<E>&) const noexcept = default;
+};
+
+template<typename E>
+constexpr E operator&(E l, E r) {
+    using U = std::underlying_type_t<E>;
+    return static_cast<E>(
+        static_cast<U>(l) & static_cast<U>(r)
+    );
+}
+
+template<typename E>
+constexpr E operator&(E l, Flags<E> r) {
+    return r & l;
+}
+
+template<typename E>
+constexpr Flags<E> operator|(E l, E r) {
+    using U = std::underlying_type_t<E>;
+    return static_cast<E>(
+        static_cast<U>(l) | static_cast<U>(r)
+    );
+}
+
+template<typename E>
+constexpr Flags<E> operator|(E l, Flags<E> r) {
+    return r | l;
+}
+}
