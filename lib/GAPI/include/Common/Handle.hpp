a876763aa51828ea494bfa05e523a9fb92a28b6c
Michael <attractadore02@gmail.com>
2022-06-21 17:10:09 +0300

    [Common]: Add custom deleter deduction guide to Handle

--- lib/GAPI/include/Common/Handle.hpp
+++ lib/GAPI/include/Common/Handle.hpp
@@ -2,6 +2,16 @@
 #include <memory>
 
 namespace R1 {
+namespace Detail {
 template<typename H, typename D>
-using Handle = std::unique_ptr<std::remove_pointer_t<H>, D>;
+using HandleBase = std::unique_ptr<std::remove_pointer_t<H>, D>;
+}
+
+template<typename H, typename D>
+struct Handle: Detail::HandleBase<H, D> {
+    using Detail::HandleBase<H, D>::HandleBase;
+};
+
+template<typename H, typename D>
+Handle(H, D&&) -> Handle<H, std::remove_cvref_t<D>>;
 }
